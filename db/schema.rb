d9e79bf1386cc51308d78ed226eeb15f1cf20516
Yongjian Si <si.yongjian@gmail.com>
2014-07-24 21:05:32 -0700

    Revert "Fixed Database column errors- at original state"
    This reverts commit b7da4df0cb0ec7f46cba69b1882a364407ed7555.
    
    Conflicts:
    	db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20140725002505) do
+=======
+ActiveRecord::Schema.define(version: 20140718232136) do
+>>>>>>> parent of b7da4df... Fixed Database column errors- at original state
 
   create_table "users", force: true do |t|
     t.string   "name"
